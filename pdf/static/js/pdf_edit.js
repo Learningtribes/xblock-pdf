8e2de3ca179c2a7d1c809c8cbae0a3e5e8871e32
rythmE <poiuwertion@gmail.com>
2020-04-22 16:58:43 +0800

    PDF Component V2

--- pdf/static/js/pdf_edit.js
+++ pdf/static/js/pdf_edit.js
@@ -2,6 +2,7 @@
 function pdfXBlockInitEdit(runtime, element) {
     $(element).find('.action-cancel').bind('click', function () {
         runtime.notify('cancel', {});
+        $('.pdf_placeholder').hide();
     });
 
     $(element).find('.action-save').bind('click', function () {
@@ -12,6 +13,10 @@ function pdfXBlockInitEdit(runtime, element) {
             'source_text': $('#pdf_edit_source_text').val(),
             'source_url': $('#pdf_edit_source_url').val()
         };
+
+        if (data.url && data.url[0] !== '/' && data.url[0] !== 'h') {
+            data.url = window.location.href.split(':')[0] + '://' + data.url;
+        }
 
         runtime.notify('save', { state: 'start' });
 
@@ -24,5 +29,137 @@ function pdfXBlockInitEdit(runtime, element) {
                 runtime.notify('error', { msg: response.message });
             }
         });
+
+        $('.pdf_placeholder').hide();
     });
+
+
+    $(element).find("#browse-button").bind('click', function (){
+        $('#file-input').click();
+    });
+    
+    var fileInput = $('#file-input')[0];
+    fileInput.onchange = function () {
+        var file = fileInput.files[0];
+        console.log('file input', file);
+        var formData = new FormData();
+        formData.append('file', file);
+        var request = new XMLHttpRequest();
+        var course_key = element[0].dataset['usageId'].split('+', 3).join('+').replace('block', 'course');
+        var upload_url = '/assets/' + course_key + '/';
+        var csrftoken = $.cookie('csrftoken');
+        var alertField = $('.alert-field');
+        request.open("POST", upload_url);
+        request.setRequestHeader("X-CSRFToken", csrftoken);
+        request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        request.setRequestHeader('Accept', 'application/json');
+        alertField.removeClass('hidden');
+        alertField.addClass('alert-info');
+        alertField.removeClass('alert-success');
+        alertField.removeClass('alert-error');
+        $('#alert-info-text').removeClass('hidden');
+        $('#alert-success-text').addClass('hidden');
+        $('#alert-error-text').addClass('hidden');
+        request.onerror = function () {
+           alertField.removeClass('alert-info');
+           alertField.removeClass('alert-success');
+           alertField.addClass('alert-error');
+           $('#alert-info-text').addClass('hidden');
+           $('#alert-success-text').addClass('hidden');
+           $('#alert-error-text').removeClass('hidden');
+        };
+        request.onload = function () {
+            var response = JSON.parse(request.responseText);
+            if (request.status === 200) {
+                $('#pdf_edit_url').val(response.asset.portable_url);
+               alertField.removeClass('alert-info');
+               alertField.addClass('alert-success');
+               alertField.removeClass('alert-error');
+               $('#alert-info-text').addClass('hidden');
+               $('#alert-success-text').removeClass('hidden');
+               $('#alert-error-text').addClass('hidden');
+            } else {
+               alertField.removeClass('alert-info');
+               alertField.removeClass('alert-success');
+               alertField.addClass('alert-error');
+               $('#alert-info-text').addClass('hidden');
+               $('#alert-success-text').addClass('hidden');
+               $('#alert-error-text').removeClass('hidden');
+            }
+        };
+        request.send(formData);
+    };
+
+    var dropZone = $('.drop-zone')[0];
+    $('.pdf_placeholder').show();
+
+    dropZone.ondragenter = function() {
+      dropZone.style.background = "rgba(0, 240, 240, 0.2)";
+    };
+    dropZone.ondragover = function() {
+      dropZone.style.background = "rgba(0, 240, 240, 0.2)";
+      return false;
+    };
+    dropZone.ondragleave = function() {
+      dropZone.style.background = "white";
+    };
+    dropZone.ondrop = function(ev) {
+      var oFile = ev.dataTransfer.files[0];
+      var request = new XMLHttpRequest();
+      var reader = new FileReader();
+      var course_key = element[0].dataset['usageId'].split('+', 3).join('+').replace('block', 'course');
+      var upload_url = '/assets/' + course_key + '/';
+      var csrftoken = $.cookie('csrftoken');
+      var alertField = $('.alert-field');
+      reader.readAsDataURL(oFile, 'base64');
+      var formData = new FormData();
+      formData.append("file", oFile);
+      reader.onload = function(e) {
+        request.open("POST", upload_url);
+        request.setRequestHeader("X-CSRFToken", csrftoken);
+        request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+        request.setRequestHeader('Accept', 'application/json');
+        alertField.removeClass('hidden');
+       alertField.addClass('alert-info');
+       alertField.removeClass('alert-success');
+       alertField.removeClass('alert-error');
+       $('#alert-info-text').removeClass('hidden');
+       $('#alert-success-text').addClass('hidden');
+       $('#alert-error-text').addClass('hidden');
+        request.onerror = function () {
+           alertField.removeClass('alert-info');
+           alertField.removeClass('alert-success');
+           alertField.addClass('alert-error');
+           $('#alert-info-text').addClass('hidden');
+           $('#alert-success-text').addClass('hidden');
+           $('#alert-error-text').removeClass('hidden');
+        };
+        request.onload = function () {
+            var response = JSON.parse(request.responseText);
+            if (request.status === 200) {
+                $('#pdf_edit_url').val(response.asset.portable_url);
+               alertField.removeClass('alert-info');
+               alertField.addClass('alert-success');
+               alertField.removeClass('alert-error');
+               $('#alert-info-text').addClass('hidden');
+               $('#alert-success-text').removeClass('hidden');
+               $('#alert-error-text').addClass('hidden');
+            } else {
+               alertField.removeClass('alert-info');
+               alertField.removeClass('alert-success');
+               alertField.addClass('alert-error');
+               $('#alert-info-text').addClass('hidden');
+               $('#alert-success-text').addClass('hidden');
+               $('#alert-error-text').removeClass('hidden');
+            }
+        };
+        request.send(formData);
+      };
+      dropZone.style.background = "white";
+      return false;
+    };
+    
+    $('#alert-field-close')[0].onclick = function () {
+        $('.alert-field').addClass('hidden');
+    }
 }
