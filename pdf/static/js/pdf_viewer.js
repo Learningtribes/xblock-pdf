157b327e1eb893307d2d9f0e3a06664895041e51
Yang <somarl@live.com>
2025-08-28 10:43:49 +0800

    fix option to allow/prevent download (#17)

--- pdf/static/js/pdf_viewer.js
+++ pdf/static/js/pdf_viewer.js
@@ -8,7 +8,13 @@ function pdfXBlockInitView(runtime, element, json) {
         element = $(element);
     }
 
-    element.find(".pdf-h5-container[data='']").attr('data', '/xblock/resources/pdf/public/web/viewer.html?file='+json.url)
+    const $element = element[0]
+    $element.querySelectorAll(".pdf-h5-container[data='']").forEach($c => {
+        $c.setAttribute('data', `/xblock/resources/pdf/public/web/viewer.html?file=${($c.dataset.src && $c.dataset.src[0] === '/') ? $c.dataset.src : json.url}`)
+    })
+    $element.querySelectorAll(".pdf-embed-container[data='']").forEach($c => {
+        $c.setAttribute('data', `${$c.dataset.src}#view=FitH&toolbar=${json.allow_download ? 1 : 0}`)
+    })
 
     $(function () {
         element.find('.pdf-download-button').on('click', function () {
